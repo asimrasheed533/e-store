cc34a7d5b48f70e7c5b92508e87e18bec7656dd1
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-03 09:08:57 +0500

    base

---
+++ app/components/Header.jsx
@@ -0,0 +1,153 @@
+import React, { useEffect, useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
+import "@/style/header.scss";
+import { useRouter } from "next/navigation";
+import { NavLink } from "react-router";
+
+export default function Header() {
+  const router = useRouter();
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  function checkNavOpen() {
+    if (window.innerWidth <= 950) {
+      setIsOpenMenu(false);
+    } else {
+      setIsOpenMenu(true);
+    }
+  }
+  useEffect(() => {
+    checkNavOpen();
+
+    window.addEventListener("resize", checkNavOpen);
+  }, []);
+
+  return (
+    <>
+      <ClickAwayListener
+        onClickAway={() => {
+          if (window.innerWidth <= 950) {
+            setIsOpenMenu(false);
+          }
+        }}
+      >
+        <div className="nav__bar__container">
+          {isOpenMenu ? (
+            <div className="nav__entry">
+              <NavLink
+                to="/"
+                className={`nav__entry__item ${
+                  pathname === "/" ? "active" : ""
+                }`}
+              >
+                Home
+              </NavLink>
+
+              <>
+                <NavLink to="/order" className="nav__entry__item">
+                  Order Now
+                </NavLink>
+                <NavLink to="/reviews" className="nav__entry__item">
+                  Reviews
+                </NavLink>
+              </>
+
+              <NavLink to="/contact" className="nav__entry__item">
+                Contact
+              </NavLink>
+              <div className="menu__close__warper">
+                <div className="menu__close__title">Menu</div>
+                <div
+                  onClick={() => setIsOpenMenu(false)}
+                  className="menu__close"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-x"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          ) : null}
+          <button
+            onClick={() => setIsOpenMenu(!isOpenMenu)}
+            className="header__container__menu__icon"
+          >
+            <svg
+              width="24"
+              height="22"
+              viewBox="0 0 14 7"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line y1="0.5" x2="7" y2="0.5" stroke="#C4C4C4" />
+              <line y1="3.5" x2="11" y2="3.5" stroke="#C4C4C4" />
+              <line y1="6.5" x2="14" y2="6.5" stroke="#C4C4C4" />
+            </svg>
+          </button>
+          <button className="nav__log">
+            <img
+              width={100}
+              height={100}
+              className="nav__log__image"
+              src="#"
+              alt="logo"
+            />
+          </button>
+          {/* <div className="nav__register">
+            {token ? (
+              <>
+                <button
+                  onClick={() => router.push("/signIn")}
+                  className="nav__register__button__svg"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-user"
+                  >
+                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </button>
+                <button
+                  onClick={() => router.push("/dashboard")}
+                  className="nav__register__button"
+                >
+                  My Account
+                </button>
+              </>
+            ) : (
+              <>
+                <button
+                  onClick={() => router.push("/signIn")}
+                  className="nav__register__button"
+                >
+                  Login
+                </button>
+              </>
+            )}
+          </div> */}
+          <button className="nav__register__button">Login</button>
+        </div>
+      </ClickAwayListener>
+    </>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,179 @@
+import banner1 from "../../assets/banner_1.jpg";
+import banner from "../../assets/burger.webp";
+import banner_2 from "../../assets/banner_2.jpg";
+import fast01 from "../../assets/fast01.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 export default function Home() {
-  return <div>home</div>;
+  const products = [
+    {
+      id: 1,
+      title: "Family Platter",
+      subtitle: "1 whole Chicken, 5 Wings",
+      price: 30.2,
+      imgSrc: banner,
+    },
+    {
+      id: 2,
+      title: "Veggie Delight",
+      subtitle: "2 Veggie Wraps, Fries",
+      price: 35.5,
+      imgSrc: banner_2,
+    },
+    {
+      id: 3,
+      title: "Veggie Delight",
+      subtitle: "2 Veggie Wraps, Fries",
+      price: 35.5,
+      imgSrc: banner_2,
+    },
+    {
+      id: 4,
+      title: "Veggie Delight",
+      subtitle: "2 Veggie Wraps, Fries",
+      price: 35.5,
+      imgSrc: banner,
+    },
+  ];
+
+  return (
+    <>
+      <div className="page__title__row">
+        <div className="page__title__col">
+          <img
+            className="page__title__col__img"
+            width={1000}
+            height={70}
+            src={banner}
+            alt="food"
+          />
+        </div>
+        <div className="page__title__col">
+          <div className="page__title__col__heading">Open</div>
+          <div className="page__title__col__subheading">
+            Take any order at the moment
+          </div>
+        </div>
+        <div className="page__title__col">
+          <img
+            width={1000}
+            height={170}
+            className="page__title__col__img"
+            src={banner}
+            alt="food"
+          />
+        </div>
+      </div>
+      <Swiper
+        className="Swiper__container"
+        modules={[Navigation]}
+        navigation={true}
+      >
+        <SwiperSlide>
+          <img
+            width={1000}
+            height={500}
+            className="page__banner__img"
+            src={banner1}
+            alt="food"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            width={1000}
+            height={100}
+            className="page__banner__img"
+            src={banner1}
+            alt="food"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            width={1000}
+            height={100}
+            className="page__banner__img"
+            src={banner1}
+            alt="food"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <div className="product__container">
+        <div className="product__main__text__title">Most Popular Food</div>
+
+        <div className="product__main__cards">
+          {products.map((product) => (
+            <div key={product.id} className="product__main__card">
+              <div className="product__main__card__img">
+                <img
+                  width={100}
+                  height={100}
+                  className="product__main__card__img__img"
+                  src={product.imgSrc}
+                  alt={product.title}
+                />
+              </div>
+              <div className="product__main__card__title">{product.title}</div>
+              <div className="product__main__card__sub__title">
+                {product.subtitle}
+              </div>
+              <div className="product__main__card__price__warper">
+                <div className="products__price">
+                  ${product.price.toFixed(2)}
+                </div>
+                <button className="products__add">
+                  <svg
+                    width="40"
+                    height="40"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-plus"
+                  >
+                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="fast__delivery__warper">
+        <div className="fast__delivery__entry">
+          <div className="fast__delivery__entry__svg">
+            <img width={100} height={100} src={fast01} alt="fastFood" />
+          </div>
+          <div className="fast__delivery__entry__title">Fast Delivery</div>
+          <div className="fast__delivery__entry__subTitle">
+            Fastest Delivery
+          </div>
+        </div>
+        <div className="fast__delivery__entry">
+          <div className="fast__delivery__entry__svg">
+            <img width={100} height={100} src={fast01} alt="fastFood" />
+          </div>
+          <div className="fast__delivery__entry__title">
+            Easy Payment Method
+          </div>
+          <div className="fast__delivery__entry__subTitle">
+            Fastest Delivery
+          </div>
+        </div>
+        <div className="fast__delivery__entry">
+          <div className="fast__delivery__entry__svg">
+            <img src={fast01} width={100} height={100} alt="fastFood" />
+          </div>
+          <div className="fast__delivery__entry__title">Fast Delivery</div>
+          <div className="fast__delivery__entry__subTitle">
+            Fastest Delivery
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
