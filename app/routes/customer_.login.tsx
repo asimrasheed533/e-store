bf706d426a1c047bdfbab3b3ac441d22c660e814
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-09 11:35:42 +0500

    Update customer_.login.tsx

--- app/routes/customer_.login.tsx
+++ app/routes/customer_.login.tsx
@@ -1,5 +1,46 @@
-import React from "react";
-
 export default function CustomerLogin() {
-  return <div>CustomerLogin</div>;
+  return (
+    <>
+      <div className="login__container">
+        <div className="login__container__image">
+          <img
+            className="login__container__image__img"
+            src="/loginImg.jpg"
+            alt="fast food"
+          />
+        </div>
+        <div className="login__content">
+          <form className="login__content__form" action="" method="post">
+            <div className="login__content__form__field">
+              <label htmlFor="email">Email</label>
+              <input
+                className="login__content__form__input"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="login__content__form__field">
+              <label htmlFor="password">Password</label>
+              <input
+                className="login__content__form__input"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className="login__content__form__field">
+              <input
+                className="login__content__form__button"
+                type="submit"
+                value="Login"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
