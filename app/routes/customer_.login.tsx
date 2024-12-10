ac307c15796cd2198e5c5ecf1ab1f3733b341ff3
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-10 16:03:08 +0500

    Update customer_.login.tsx

--- app/routes/customer_.login.tsx
+++ app/routes/customer_.login.tsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
-import { Link } from "react-router";
+import { ActionFunctionArgs, Link, redirect, useFetcher } from "react-router";
 import Input from "~/components/Input";
 import loginImg from "../../assets/loginImg.png";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const body = await request.formData();
+  const email = body.get("email");
+  const password = body.get("password");
+  console.log(email, password);
+  return redirect("/customer");
+}
+
 export default function CustomerLogin() {
+  const fetcher = useFetcher();
   return (
     <>
       <div className="login__container">
@@ -15,18 +24,20 @@ export default function CustomerLogin() {
         </div>
         <div className="login__content">
           <div className="login__content__title">Welcome Royal Chef</div>
-          <form className="login__content__form" action="" method="post">
+          <fetcher.Form className="login__content__form" method="post">
             <Input label="Enter Email" name="email" />
             <Input label="Enter Password" name="password" />
 
             <div className="login__content__form__field">
-              <input
+              <button
                 className="login__content__form__button"
                 type="submit"
                 value="Login"
-              />
+              >
+                Login
+              </button>
             </div>
-          </form>
+          </fetcher.Form>
           <Link to="/customer/signUp" className="go__to__signUp">
             SignUp
           </Link>
