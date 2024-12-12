d1bdbb1aaf065e8a93dcb17d21e0294f7a665e56
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-12 17:52:28 +0500

    fixs style

--- app/root.tsx
+++ app/root.tsx
@@ -21,9 +21,8 @@ export function meta() {
   ];
 }
 
-const styles = import.meta.glob("/style/**/*.scss", {
+import.meta.glob("/style/**/*.scss", {
   eager: true,
-  query: "url",
 });
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -36,10 +35,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  ...Object.keys(styles).map((style) => ({
-    rel: "stylesheet",
-    href: style,
-  })),
+  // ...Object.keys(styles).map((style) => ({
+  //   rel: "stylesheet",
+  //   href: style,
+  // })),
 ];
 
 import type { Route } from "./+types/root";
