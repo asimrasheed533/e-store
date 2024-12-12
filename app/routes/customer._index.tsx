00893e68a4ceb2b566c92001fc155bcea00ce3b9
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-12 17:44:43 +0500

    Update customer._index.tsx

--- app/routes/customer._index.tsx
+++ app/routes/customer._index.tsx
@@ -1,3 +1,15 @@
+import { redirect } from "react-router";
+import authCheck from "utils/authCheck";
+import { Route } from "./+types/customer._index";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const token = await authCheck(request);
+
+  if (!token) {
+    return redirect("/customer/login");
+  }
+}
+
 export default function customerDashboard() {
   return <div>customerDashboard</div>;
 }
